35901b3c731a2600801be1b8636f43027bd7ee73
DESKTOP-1SK3R1N\Wylliam <carloswylliam023@gmail.com>
2023-09-12 19:54:37 -0300

    export knex

--- src/conexao/conexao.js
+++ src/conexao/conexao.js
@@ -1,5 +1,5 @@
 const knex = require('knex')({
-    client: 'mysql',
+    client: 'pg',
     connection: {
       host : process.env.DB_HOST,
       port : process.env.DB_PORT,
@@ -9,3 +9,5 @@ const knex = require('knex')({
     },
     ssl: {rejectUnauthorized: false}
   })
+
+module.exports = knex
