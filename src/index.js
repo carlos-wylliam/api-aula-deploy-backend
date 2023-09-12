26e9c94f37acf5d01b5d460a68a5c0e98e886744
DESKTOP-1SK3R1N\Wylliam <carloswylliam023@gmail.com>
2023-09-12 20:45:42 -0300

    configurando banco de dados com o cyclic

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
-
 const express = require('express')
-
 const knex = require('knex')
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
